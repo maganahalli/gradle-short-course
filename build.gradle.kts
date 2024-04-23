d30fa64a021adec42f954d5775b88ed5b660eaa9
Venky <maganahalli@gmail.com>
2024-04-23 15:02:22 -0400

    fixed buid.gradle.kts file for Junit5

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,19 @@
     java
  }
 
- tasks.named<Jar>("jar"){
-    manifest  {
-        attributes["Main-Class"] = "com.test.first.SayHello";
-    }
-
-    repositories{
-      mavenCentral()
-    }
-
-    dependencies{
-      testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-    }
-
+ repositories{
+     mavenCentral()
  }
+ dependencies{
+     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+ }
+ tasks.named<Jar>("jar") {
+     manifest {
+         attributes["Main-Class"] = "com.test.first.SayHello";
+     }
+ }
+
+ tasks.named<Test>("test") {
+     useJUnitPlatform()
+ }
+
